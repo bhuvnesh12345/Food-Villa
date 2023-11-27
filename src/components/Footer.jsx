0f4bc7558b5ff44de41bc1b1ae934b582a3a19b8
bhuvnesh12345 <64630728+bhuvnesh12345@users.noreply.github.com>
2023-11-27 22:45:21 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,11 +3,8 @@ const Footer = () => {
     <div className='container-max text-center py-4 border-t'>
       Foody, made by{' '}
       <a
-        href='https://dineshrout.netlify.app'
-        target={'_blank'}
-        className='text-orange-500'
       >
-        Dinesh Rout
+        Bhuvnesh
       </a>
     </div>
   );
